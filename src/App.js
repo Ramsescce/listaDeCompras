b64a8e558c5027e6dae53f19300278bd2954b319
Ramses Godoy <74273214+Ramsescce@users.noreply.github.com>
2021-01-21 16:17:36 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import ListaDeCompras from './ListadeCompras'
+function App() {
+  return (
+    <div className="App">
+      <ListaDeCompras nome="arroz"/>
+      <ListaDeCompras nome="feijao"/>
+      <ListaDeCompras nome="canela"/>
+      <ListaDeCompras nome="cebola"/>
+      <ListaDeCompras nome="azeitona"/>
+      
+        
+      
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ListadeCompras.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function ListaDeCompras (item){
+    
+return(
+<h1>{item.nome}  </h1> )
+}   
+export default ListaDeCompras
+
+
